dc92c9751469280174f5b54126687a33ab7d6106
Stelo <stelo@Bruno-2.local>
2019-03-01 14:01:51 -0300

    included unit test

--- src/test/java/com/nexaas/DesafioProgramacaoApplicationTests.java
+++ src/test/java/com/nexaas/DesafioProgramacaoApplicationTests.java
@@ -1,16 +1,49 @@
 package com.nexaas;
 
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.nexaas.domain.File;
+import com.nexaas.exception.FileStorageException;
+import com.nexaas.service.FileStorageService;
+import com.nexaas.utils.Converter;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.StringUtils;
+
+import java.io.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DesafioProgramacaoApplicationTests {
 
+	@Autowired
+	private FileStorageService service;
+
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testTotalGross() throws Exception {
+
+		FileInputStream inputFile = new FileInputStream("uploads/example_input.tab");
+		MockMultipartFile file = new MockMultipartFile("file", inputFile);
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+		BufferedReader buf = null;
+
+		String sum = service.storeFile(file);
+
+		Assert.assertEquals("R$ 30,00",sum);
+	}
 }
